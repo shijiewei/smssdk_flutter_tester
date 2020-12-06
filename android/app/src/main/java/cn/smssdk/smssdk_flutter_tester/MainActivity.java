005fcafcaf19121afaf18abe0502929d5b95167e
shijiewei <jackiewayne2046@163.com>
2020-12-06 23:07:09 +0800

    集成短信与公共库

--- android/app/src/main/java/cn/smssdk/smssdk_flutter_tester/MainActivity.java
+++ android/app/src/main/java/cn/smssdk/smssdk_flutter_tester/MainActivity.java
@@ -1,6 +1,28 @@
 package cn.smssdk.smssdk_flutter_tester;
 
+import android.os.Bundle;
+
+import com.mob.MobSDK;
+
+import cn.smssdk.flutter.MobsmsPlugin;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+	// 通过Mob console申请获得
+	private static final String MOB_APPKEY = "moba6b6c6d6";
+	private static final String MOB_APPSECRET = "b89d2427a3bc7ad1aea1e1e8c1d36bf3";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// 初始化SMSSDK
+		MobSDK.init(this, MOB_APPKEY, MOB_APPSECRET);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// 执行回收操作
+		MobsmsPlugin.recycle();
+	}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,314 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import 'package:flutter/services.dart';
+import 'package:mobsms/mobsms.dart' ;
+import 'package:mobcommonlib/mobcommonlib.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+
+  var zoneController = new TextEditingController();
+  var phoneController = new TextEditingController();
+  var tempIDController = new TextEditingController();
+  var codeController = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('SMSSDK Flutter'),
+        ),
+        body: new Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            child: new ListView.builder(
+              itemCount: 1,
+              itemBuilder: (context, i)=> renderRow(i,context),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void showAlert(String text, BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+            title: new Text("提示"),
+            content: new Text(text),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text("OK"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ]));
+  }
+
+  void showPrivacyAlert(String text, BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+            title: new Text("隐私协议"),
+            content: new Text(text),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text("同意"),
+                onPressed: () {
+                  // 关闭弹框
+                  Navigator.of(context).pop();
+                  Mobcommonlib.submitPolicyGrantResult(true, (dynamic ret, Map err) => {});
+                },
+              ),
+              new FlatButton(
+                child: new Text("拒绝"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  Mobcommonlib.submitPolicyGrantResult(false, (dynamic ret, Map err) => {});
+                },
+              )
+            ]));
+  }
+
+  Widget renderRow(i, BuildContext context){
+    return new Column(
+
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          height: 30,
+        ),
+        Text('请先输入手机号'),
+        TextField(
+          controller: zoneController,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(15.0),
+            labelText: '请输入国家 如:86',
+          ),
+        ),
+        TextField(
+          controller: phoneController,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(10.0),
+              labelText: '请输入手机号'
+          ),
+        ),
+        TextField(
+          controller: tempIDController,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(10.0),
+              labelText: '模板编号（没有则不填）：'
+          ),
+        ),
+        Container(
+          height: 30,
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(minWidth: double.infinity),
+          child:new FlatButton(
+            color: Colors.blueGrey,
+            textColor: Colors.white,
+            child: new Text('请求文本验证码'),
+            onPressed: (){
+
+              Smssdk.getTextCode(phoneController.text, zoneController.text, tempIDController.text, (dynamic ret, Map err){
+
+                if(err!=null)
+                {
+                  showAlert(err.toString(),context);
+                }
+                else
+                {
+                  String rst = ret.toString();
+                  if (rst == null || rst == "") {
+                    rst = '获取验证码成功!';
+                  }
+                  showAlert(rst,context);
+                }
+              });
+
+            },
+          ),
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(minWidth: double.infinity),
+          child:new FlatButton(
+            color: Colors.blueGrey,
+            textColor: Colors.white,
+            child: new Text('请求语音验证码'),
+            onPressed: (){
+              Smssdk.getVoiceCode(phoneController.text, zoneController.text, (dynamic ret, Map err){
+                if(err!=null)
+                {
+                  showAlert(err.toString(),context);
+                }
+                else
+                {
+                  showAlert('获取验证码成功!',context);
+                }
+              });
+            },
+          ),
+        ),
+
+        TextField(
+          controller: codeController,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(10.0),
+              labelText: '请输入验证码'
+          ),
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(minWidth: double.infinity),
+          child: new FlatButton(
+            color: Colors.blueGrey,
+            textColor: Colors.white,
+            child: new Text('提交验证码'),
+            onPressed: (){
+              Smssdk.commitCode(phoneController.text, zoneController.text, codeController.text, (dynamic ret, Map err){
+                if(err!=null)
+                {
+                  showAlert(err.toString(),context);
+                }
+                else
+                {
+                  showAlert('提交验证码成功!',context);
+                }
+              });
+            },
+          ),
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(minWidth: double.infinity),
+          child: new   FlatButton(
+            color: Colors.blueGrey,
+            textColor: Colors.white,
+            child: new Text('获取国家列表'),
+            onPressed: (){
+              Smssdk.getSupportedCountries((dynamic ret, Map err){
+                if(err!=null)
+                {
+                  showAlert(err.toString(),context);
+                }
+                else
+                {
+                  showAlert(ret.toString(),context);
+                }
+              });
+            },
+          ),
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(minWidth: double.infinity),
+          child: new FlatButton(
+            color: Colors.blueGrey,
+            textColor: Colors.white,
+            child: new Text('获取应用内好友'),
+            onPressed: (){
+              Smssdk.getFriends((dynamic ret, Map err){
+                if(err!=null)
+                {
+                  showAlert(err.toString(),context);
+                }
+                else
+                {
+                  showAlert(ret.toString(),context);
+                }
+              });
+            },
+          ),
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(minWidth: double.infinity),
+          child: new FlatButton(
+            color: Colors.blueGrey,
+            textColor: Colors.white,
+            child: new Text('提交用户信息'),
+            onPressed: (){
+              Smssdk.submitUserInfo("3241241", "SmsSDK_Flutter_User_3241241",
+                  "http://download.sdk.mob.com/510/deb/0c0731ac543eb71311c482a2e2.png",
+                  zoneController.text, phoneController.text, (dynamic ret, Map err){
+                    if(err!=null)
+                    {
+                      showAlert(err.toString(),context);
+                    }
+                    else
+                    {
+                      showAlert('提交用户信息 成功!',context);
+                    }
+                  });
+            },
+          ),
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(minWidth: double.infinity),
+          child: new FlatButton(
+            color: Colors.blueGrey,
+            textColor: Colors.white,
+            child: new Text('获取版本号'),
+            onPressed: (){
+
+              Smssdk.getVersion((dynamic ret, Map err){
+                if(err!=null)
+                {
+                  showAlert(err.toString(),context);
+                }
+                else
+                {
+                  showAlert(ret.toString(),context);
+                }
+              });
+            },
+          ),
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(minWidth: double.infinity),
+          child: new FlatButton(
+            color: Colors.blueGrey,
+            textColor: Colors.white,
+            child: new Text('开启提示框'),
+            onPressed: (){
+              Smssdk.enableWarn(true,(dynamic ret, Map err){
+                if(err!=null)
+                {
+                  showAlert(err.toString(),context);
+                }
+                else
+                {
+                  showAlert('开启提示框 成功!',context);
+                }
+              });
+            },
+          ),
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(minWidth: double.infinity),
+          child: new FlatButton(
+            color: Colors.blueGrey,
+            textColor: Colors.white,
+            child: new Text('打开隐私协议弹框'),
+            onPressed: (){
+              showPrivacyAlert('是否同意隐私协议？',context);
+            },
+          ),
+        ),
+      ],
     );
   }
 }
